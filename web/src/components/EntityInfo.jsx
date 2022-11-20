418d906ed210c8f0e82fe3631b45a9b77c46dc48
Akionka <42269025+Akionka@users.noreply.github.com>
2022-11-20 23:00:51 +0300

    Added entity information component

---
+++ web/src/components/EntityInfo.jsx
@@ -0,0 +1,16 @@
+import { Button, Paper, Stack, Typography } from '@mui/material';
+
+export const EntityInfo = ({ onDelete, items }) => {
+  return  <Stack spacing={1}>
+    <Paper elevation={0}>
+      {items.map((item) => (
+        <Typography>
+          {item.label}: {item.value}
+        </Typography>
+      ))}
+    </Paper>
+    <Button variant="contained" onClick={onDelete}>
+      Удалить
+    </Button>
+  </Stack>;
+};
